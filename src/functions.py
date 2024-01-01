a9f9bb04424cd0040be4898b9bb5ff66efa83a3c
czwz <tim010007@gmail.com>
2024-01-01 20:47:25 +0800

    change ProcessUserInput definition

--- src/functions.py
+++ src/functions.py
@@ -5,7 +5,7 @@ from .base import Action, Context, Message, Role
 from .roles import User
 
 
-logger = Logger()
+logger = Logger(name="autobot")
 
 
 class Think(Action):
@@ -17,18 +17,17 @@ class GetReply(Action):
 
 
 class ProcessUserInput(Action):
-    def __init__(self, command: str) -> None:
-        """create an action to process the input"""
-        self.command = command
-        self.name = "process the input from user."
-        self.description = (
-            "this action will decide"
-            "to think, or to update the history"
-            "depending on the type of user input."
-        )
-    
+    name: str = "process the input from user."
+    description: str = (
+        "this action will decide"
+        "to think, or to update the history"
+        "depending on the type of user input."
+    )
+
+    @classmethod
     def execute(
-        self,
+        cls,
+        command: str,
         role: Role,
         to_do: List[Tuple[Role, Action]],
         context: Context
@@ -37,8 +36,8 @@ class ProcessUserInput(Action):
         logger.info("Get the input from the user, start processing ...")
         if to_do and isinstance(to_do[-1][1], GetReply):
             logger.info("the input should be a reply, let's note it down ...")
-            message = Message(role=User, content=self.command)
+            message = Message(role=User, content=command)
             context.history.append(message)
         else:
             logger.info("the input is a command, let's think how to do it ...")
-            to_do.append((role, Think(command=self.command)))
+            to_do.append((role, Think(command=command)))
